c46f2ee7c93e06b902b048e925ce12ac2285ab61
gabriella garcia <gabirvargas@gmail.com>
2023-07-08 11:54:11 -0300

    delete and link_to add

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,17 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  ## Read all
+  # get "restaurants", to: "restaurants#index"
+  ## Create
+  # get "restaurants/new", to: "restaurants#new"
+  # post "restaurants", to: "restaurants#create"
+  ## Read one - The `show` route needs to be *after* `new` route.
+  # get "restaurants/:id", to: "restaurants#show", as: "restaurant"
+  ## Update
+  # get "restaurants/:id/edit", to: "restaurants#edit"
+  # patch "restaurants/:id", to: "restaurants#update"
+  ## Delete
+  # delete "restaurants/:id", to: "restaurants#destroy"
+  resources :restaurants
 end
